bc8ea3b4e7ff55e647448926ec553dbd945d4e26
Peter Krieg <copperkrieg@gmail.com>
2014-12-21 03:29:20 -0500

    somehow fixed the scrolling problem, wow.  no idea how, tired and bedtime

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -734,24 +734,6 @@ $(document).ready(function () {
 });
 
 
-/*===================================================================================*/
-/*	GOOGLE MAPS
-/*===================================================================================*/
-
-$(document).ready(function () {
-	
-	function initialize() {
-		var mapOptions = {
-			zoom: 13,
-			center: new google.maps.LatLng(40.7902778, -73.9597222),
-			disableDefaultUI: true
-		}
-		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-	}
-	
-	google.maps.event.addDomListener(window, 'load', initialize);
-	
-});
 
 
 /*===================================================================================*/
