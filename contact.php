2a03e6d7a90a7dbc3782ee02b7d16b2a97d6c9a4
Peter Krieg <copperkrieg@gmail.com>
2014-12-22 19:43:12 -0500

    got contact form to send email, but it will not contain info from contaxt box.. so pretty useless still, fix later

--- contact.php
+++ contact.php
@@ -1,19 +1,48 @@
 <?php
-	// ========== Enter your e-mail address here ========== //
-	$to = 'copperkrieg@gmail.com';
-	
-	$name=$_REQUEST['name'];
-	$email=$_REQUEST['email'];
-	$subject = $_REQUEST['subject'];
-	
-	if($subject==""){
-		$subject = 'Question About Summer Destinations';
-	}
-	
-	$message=$_REQUEST['message'];
-	
-	$body = "Name: \t$name \nEmail: \t$email \n\nMessage: \n\n$message";
-	$headers = 'From: '.$name.' <'.$email.'>' . "\r\n" . 'Reply-To: ' . $email;
-	
-	mail($to, $subject, $body, $headers);
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+$subject = $_POST['cf_subject'];
+
+
+
+
+
+
+
+$mail_to = 'pkrieg2@gmail.com';
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message."\n";
+
+
+// $headers = 'From: '.$field_email."\r\n";
+// $headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+
+
+
+
+
+// Below stuff isn't completely necessary
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message! We will contact you shortly.');
+		window.location = 'http://peterkrieg.com/sdabroadtest'
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to pkrieg2@gmail.com');
+		window.location = 'http://peterkrieg.com/sdabroadtest'
+	</script>
+<?php
+}
+
+
 ?>
